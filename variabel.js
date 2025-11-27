440f200318b8733b534013293cd01a488a915f30
balyakhikam <balyanailul@gmail.com>
2025-11-27 18:29:32 +0700

    first commit

---
+++ variabel.js
@@ -0,0 +1,31 @@
+//number 
+var angka = 10;
+console.log(angka);
+
+//string
+var nama = "balyakhikam";
+console.log(nama);
+
+//boolean
+var benar = true;
+console.log(benar);
+
+//array
+var buah = ["apel","mangga","jeruk"];
+console.log(buah);
+
+//object
+var mobil = {
+    merk : "BMW",
+    tipe : "e30",
+    tahun : 1990
+};
+console.log(mobil);
+
+//underfined
+var kosong
+console.log(kosong);
+
+//null
+var nullVar = null;
+console.log(nullVar);
