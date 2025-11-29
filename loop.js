c4a5212616237723aa0901df69496675dee7a46e
balyakhikam <balyanailul@gmail.com>
2025-11-29 13:26:20 +0700

    first commit

---
+++ loop.js
@@ -0,0 +1,19 @@
+//for loop example
+for(i = 1; i < 10; i++) {
+    console.log(i);
+}
+
+//while loop example
+while(i < 10) {
+    console.log(i);
+    i++;
+}
+
+//nested loop example
+for (i = 1; i <= 5; i++); {
+    var temp = "";
+    for (j = 1; j <= i; J++) {
+        temp += "*";
+    }
+    console.log(temp);
+}
